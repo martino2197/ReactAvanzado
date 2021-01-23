79a0bc7835fe4762a549c5ef54c551d47e90d3f5
martino2197 <martino2197@gmail.com>
2021-01-23 14:59:08 -0600

    Preparando el entorno de desarrollo: Instalación de React y Babel

--- curso-platzi-react-avanzado/webpack.config.js
+++ curso-platzi-react-avanzado/webpack.config.js
@@ -4,5 +4,23 @@ module.exports = {
   output: {
     filename: "app.bundle.js",
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/, //tiene que utilizar para todos los .js
+        exclude: /node_modules/, //excluimos los archivos de node_modules
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
 };
