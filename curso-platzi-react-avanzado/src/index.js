5f43d3bd50c0a18a98fbf670b21de2f45b434161
martino2197 <martino2197@gmail.com>
2021-01-28 15:33:05 -0600

    Preparando el entorno de desarrollo: Linter, extensiones y deploy con Vercel

--- curso-platzi-react-avanzado/src/index.js
+++ curso-platzi-react-avanzado/src/index.js
@@ -1,4 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Seguimos con el curso avanzado!</h1>, document.getElementById("app"));
+ReactDOM.render(<h1>Seguimos con el curso avanzado!!!</h1>,
+  document.getElementById('app'))
--- curso-platzi-react-avanzado/webpack.config.js
+++ curso-platzi-react-avanzado/webpack.config.js
@@ -1,26 +1,26 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-  //el output de la compilacion va a estar
+  // el output de la compilacion va a estar
   output: {
-    filename: "app.bundle.js",
+    filename: 'app.bundle.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
+      template: 'src/index.html'
+    })
   ],
   module: {
     rules: [
       {
-        test: /\.js$/, //tiene que utilizar para todos los .js
-        exclude: /node_modules/, //excluimos los archivos de node_modules
+        test: /\.js$/, // tiene que utilizar para todos los .js
+        exclude: /node_modules/, // excluimos los archivos de node_modules
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-    ],
-  },
-};
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
