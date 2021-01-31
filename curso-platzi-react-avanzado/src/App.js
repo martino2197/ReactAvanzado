cdc8a2dca970beebaaec667929f5076e9cc6240b
martino2197 <martino2197@gmail.com>
2021-01-31 13:37:11 -0600

    Creando la interfaz con styled-components: Creando ListOfCategories y estilos globales

--- curso-platzi-react-avanzado/src/App.js
+++ curso-platzi-react-avanzado/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import { Category } from './components/Category'
-// import { originalName as alias } from 'module'
-// import ReactDOM from 'react-dom'
+// import { Category } from './components/Category'
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyle } from './GlobalStyles'
 
 export const App = () => (
-  <Category />
+  <>
+    <GlobalStyle />
+    <ListOfCategories />
+  </>
 )
