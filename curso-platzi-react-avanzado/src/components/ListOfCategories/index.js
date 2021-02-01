6f4e9b7af9279266758526c8c4c0362a3424d2e7
martino2197 <martino2197@gmail.com>
2021-02-01 13:24:53 -0600

    Hooks: Qué son los Hooks?

--- curso-platzi-react-avanzado/src/components/ListOfCategories/index.js
+++ curso-platzi-react-avanzado/src/components/ListOfCategories/index.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
+// import { categories as mockCategories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  // useEffect acepta una funcion que sera la que se ejecute cada vez que se renderice el componente
+  useEffect(function () {
+    window.fetch('https://petgram-server-martino2197.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        // setCategories es el metodo para actualizar categories
+        setCategories(response)
+      })
+  }, [])
   return (
     <List>
       {
