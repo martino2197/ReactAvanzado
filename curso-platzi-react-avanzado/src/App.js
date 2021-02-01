f738ba6ce1f11281c062eadd3bd7972d5ec158d9
martino2197 <martino2197@gmail.com>
2021-02-01 09:50:16 -0600

    Creando la interfaz con styled-components: SVGR (SVG a componente de ReactJS)

--- curso-platzi-react-avanzado/src/App.js
+++ curso-platzi-react-avanzado/src/App.js
@@ -4,10 +4,12 @@ import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyles'
 // import { PhotoCard } from './components/PhotoCard'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 export const App = () => (
   <>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </>
