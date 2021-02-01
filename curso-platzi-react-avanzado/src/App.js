0806b5fdce06bf01ea9313b7bf9498335eba6876
martino2197 <martino2197@gmail.com>
2021-02-01 10:12:45 -0600

    Creando la interfaz con styled-components: Creando animaciones con keyframes

--- curso-platzi-react-avanzado/src/App.js
+++ curso-platzi-react-avanzado/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import { Category } from './components/Category'
 import { ListOfCategories } from './components/ListOfCategories'
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 // import { PhotoCard } from './components/PhotoCard'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 import { Logo } from './components/Logo'
