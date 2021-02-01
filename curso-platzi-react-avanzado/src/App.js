4ff92d26bc16b101234e90db47cefc5f326a3530
martino2197 <martino2197@gmail.com>
2021-01-31 18:53:36 -0600

    Creando la interfaz con styled-components: Creando PhotoCard y usando react-icon

--- curso-platzi-react-avanzado/src/App.js
+++ curso-platzi-react-avanzado/src/App.js
@@ -2,10 +2,13 @@ import React from 'react'
 // import { Category } from './components/Category'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyles'
+// import { PhotoCard } from './components/PhotoCard'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 
 export const App = () => (
   <>
     <GlobalStyle />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </>
 )
