bd83897fd81de8f6ffafd995af09a4ab84b5e867
martino2197 <martino2197@gmail.com>
2021-02-02 21:10:31 -0600

    Hooks: Usando el localStorage para guardar los likes.

--- curso-platzi-react-avanzado/src/components/PhotoCard/index.js
+++ curso-platzi-react-avanzado/src/components/PhotoCard/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Article, ImgWrapper, Img, Button } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -8,7 +8,19 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   // la prop ref nos permite capturar elemento del DOM, guardamos esa referencia en la constante
   const refElement = useRef(null)
   const [show, setShow] = useState(false)
-
+  // creamos la key
+  const key = `like-${id}`
+  // el estado de los likes lo guardamos en local storage
+  const [liked, setLiked] = useState(() => {
+    try {
+      const like = window.localStorage.getItem(key)
+      return JSON.parse(like)
+      // La informacion que se almacena en el localStorage es un string. Por eso cuando hacemos el cambio de !liked lo que estamos enviando es una cadena de texto true o false. Si aun asi tienen duda pueden comprobarlo con typeof like
+    } catch (error) {
+      return false
+    }
+  })
+  console.log(liked)
   // con este useEffect se esta haciendo un lazy-loader de los Articles
   useEffect(function (params) {
     Promise.resolve(typeof window.IntersectionObserver !== 'undefined'
@@ -33,6 +45,16 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
       })
   }, [refElement])
 
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
+
+  const setLocalStorage = value => {
+    try {
+      window.localStorage.setItem(key, value)
+      setLiked(value)
+    } catch (e) {
+      console.error(e)
+    }
+  }
   // if (!show) return null
   return (
     <Article ref={refElement}>
@@ -44,9 +66,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
                 <Img src={src} />
               </ImgWrapper>
             </a>
-
-            <Button>
-              <MdFavoriteBorder size='32px' /> likes!
+            <Button onClick={() => setLocalStorage(!liked)}>
+              <Icon size='32px' /> likes!
             </Button>
           </>
       }
