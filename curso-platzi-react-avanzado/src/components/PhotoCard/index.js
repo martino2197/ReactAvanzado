88f74cd4580be16523c308def5f0c68a41275bd0
martino2197 <martino2197@gmail.com>
2021-02-02 20:38:55 -0600

    Hooks: Uso de polyfill de Intersetion Observer e imports dinámicos.

--- curso-platzi-react-avanzado/src/components/PhotoCard/index.js
+++ curso-platzi-react-avanzado/src/components/PhotoCard/index.js
@@ -11,19 +11,26 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
 
   // con este useEffect se esta haciendo un lazy-loader de los Articles
   useEffect(function (params) {
-    // console.log(refElement.current)
-    const observer = new window.IntersectionObserver(function (entries) {
-      // console.log(entries)
-      const { isIntersecting } = entries[0]
-      // console.log({ isIntersecting })
-      if (isIntersecting) {
-        console.log('si')
-        setShow(true)
-        observer.disconnect()
-      }
-    })
-    observer.observe(refElement.current)
-    // se ejecuta solo cuando cambia la referencia
+    Promise.resolve(typeof window.IntersectionObserver !== 'undefined'
+      ? window.IntersectionObserver
+      : import('intersection-observer')) // lo anterior es un Import Dinamico
+      .then(() => {
+      // no tenemos que devolver nada ya que al ser un pollyfill parchea directamente el objeto window
+
+        // console.log(refElement.current)
+        const observer = new window.IntersectionObserver(function (entries) {
+        // console.log(entries)
+          const { isIntersecting } = entries[0]
+          // console.log({ isIntersecting })
+          if (isIntersecting) {
+            console.log('si')
+            setShow(true)
+            observer.disconnect()
+          }
+        })
+        observer.observe(refElement.current)
+      // se ejecuta solo cuando cambia la referencia
+      })
   }, [refElement])
 
   // if (!show) return null
