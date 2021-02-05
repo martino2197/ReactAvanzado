7091306eb166c338f9b67f148f1cfb7624c20284
martino2197 <martino2197@gmail.com>
2021-02-05 09:59:24 -0600

    GraphQL y React Apollo: Qué es GraphQL y React Apollo? INicializando React Apollo Client y primer HoC.

--- curso-platzi-react-avanzado/src/components/ListOfPhotoCards/index.js
+++ curso-platzi-react-avanzado/src/components/ListOfPhotoCards/index.js
@@ -2,10 +2,70 @@ import React from 'react'
 
 import { PhotoCard } from '../PhotoCard'
 
-export const ListOfPhotoCards = () => {
+import { graphql } from 'react-apollo'
+// gql nos va permitir hacer las querys como si fuera un string y apollo lo va a entender
+import { gql } from 'apollo-boost'
+
+// primera query
+// withPhotos nos va a permitir envolver el componente y recuperar esa informacion,
+// este patron se llama "Componente de Orden superior"
+// Ya que es una funcion que se le pasa como parametro un componente
+// y devuelve otro componente ya sea con mejoras o con props inyectadas
+const withPhotos = graphql(gql`
+query getPhotos {
+  photos {
+    id
+    categoryId
+    src
+    likes
+    userId
+    liked
+  }
+}
+`)
+
+const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
+  // console.log('====================================')
+  // console.log(props)
+  // console.log('====================================')
   return (
     <ul>
-      {[1, 2, 3, 4, 5, 6, 7].map(id => <PhotoCard key={id} id={id} />)}
+      {photos.map(photo => <PhotoCard key={photo.id} {...photo} />)}
     </ul>
   )
 }
+
+export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
+
+// Para aquellos que quieran utilizar la nueva caracteristica de Apollo Client utilizando hooks quedaria asi:
+// Metodo usando la nueva caracteristica de hooks Apollo client
+
+// import { useQuery } from '@apollo/react-hooks'
+// const getPhotos = gql`
+//   query getPhotos {
+//     photos {
+//       id
+//       categoryId
+//       src
+//       likesuserId
+//       liked
+//     }
+//   }
+// `
+
+// export const ListOfPhotoCards = () ={
+//   const { loading, error, data } = useQuery(getPhotos)
+
+//   if(loading) return <p>Cargando</p>
+//   if (error) return <p>Error</p>
+
+//   return(
+//     <ul>
+//       {data.photos.map((photoCard, id) => (
+//         <PhotoCard key={id} {...photoCard} />
+//       ))}
+//     </ul>
+//   )
+// }
+
+// npm install apollo-boost @apollo/react-hooks graphql
--- curso-platzi-react-avanzado/src/index.js
+++ curso-platzi-react-avanzado/src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost' // nos va a permitir empezar rapidamente con Apollo
+import { ApolloProvider } from 'react-apollo' // vamos envolver nuestra aplicacion para poder ocupar apollo en cualquier parte del arbol de elementos
 import { App } from './App'
 
-ReactDOM.render(<App />,
+// Esste client es el que vamos a utilizar como prop en ApolloProvider
+const client = new ApolloClient({
+  uri: 'https://petgram-server-martino2197.vercel.app/graphql' // el servidor de graphql
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('app'))
