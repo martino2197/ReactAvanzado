1c407c669a0029ecbb3cc809971cf41bddea96ca
martino2197 <martino2197@gmail.com>
2021-02-06 12:30:47 -0600

    GraphQL y React Apollo: Usar render Props para recuperar una foto.

--- curso-platzi-react-avanzado/src/App.js
+++ curso-platzi-react-avanzado/src/App.js
@@ -7,11 +7,25 @@ import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 // import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { Logo } from './components/Logo'
 
-export const App = () => (
-  <>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={2} />
-  </>
-)
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
+
+export const App = () => {
+  // recibe un parametro la query string de la barra de direcciones (window.location.search)
+  const urlParams = new window.URLSearchParams(window.location.search)
+  // para poder recuperar el detailId en la que estamos navegando
+  const detailId = urlParams.get('detail')
+  console.log('====================================')
+  console.log(detailId)
+  console.log('====================================')
+  return (
+    <>
+      <GlobalStyle />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <><ListOfCategories /><ListOfPhotoCards categoryId={1} /></>
+      }
+    </>
+  )
+}
---
+++ curso-platzi-react-avanzado/src/container/PhotoCardWithQuery.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { PhotoCard } from '../components/PhotoCard'
+
+// deprecado
+// import { gql } from 'apollo-boost'
+// import { Query } from 'react-apollo' // Query es un render especial que nos permitira utilizar Render Props
+import { gql, useQuery } from '@apollo/client'
+
+const GET_PHOTO = gql`
+query getSinglePhoto($id:ID!) {
+  photo(id:$id) {
+    id
+    categoryId
+    src
+    likes
+    userId
+    liked
+  }
+}
+`
+const getPhoto = (id) => {
+  const { loading, error, data } = useQuery(GET_PHOTO, { variables: { id } })
+  if (loading) return <p>Loading....</p>
+  if (error) return <p>Error :(</p>
+  return <PhotoCard id={id} {...data.photo} />
+}
+
+export const PhotoCardWithQuery = ({ id }) => <>{getPhoto(id)}</>
+
+// export const PhotoCardWithQuery = ({ id }) => (
+//   <Query query={query} variables={{ id }}>
+//     {
+//     ({ loading, error, data }) => {
+//       const { photo = {} } = data
+//       return <PhotoCard {...photo} />
+//     }
+//     }
+//   </Query>
+// )
